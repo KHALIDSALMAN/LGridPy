fix fuel consumption calculation

LGridPy.py
@@ -8,7 +8,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 # load_data=dem_load['Y'][560:570]
 # load_data=dem_load['Y']
 
-used_rows = list(range(50))
+used_rows = list(range(100))
 
 dem_load = pd.read_csv(r'input_data/525600 mins - Elisbetta load.csv', header=None, index_col=0, skiprows=used_rows[0], nrows=len(used_rows))
 load_data = np.asarray(dem_load).T[0]
@@ -26,7 +26,7 @@ network = Network(name='My Network',
                   frequency=60, # Hz
                   rocof_limit=-2.0, # Hz/s
                   contingency_frequency=54, # Hz
-                  timebase='hours'
+                  timebase='minutes'
                   )
 
 increase = 0        # Percentage increase
@@ -41,8 +41,8 @@ network.add_gas_generator('GT1',
                         p_nom=25.,
                         p_min_pu=0.4,
                         p_max_pu=0.9,
-                        min_uptime=1/6, # minutes
-                        min_downtime=1/2, # minutes
+                        min_uptime=10, # minutes 1/6 hours
+                        min_downtime=30, # minutes 1/2 hours
                         ramp_up_limit=0.12,
                         ramp_down_limit=0.12,
                         start_up_cost=70.,
@@ -57,8 +57,8 @@ network.add_gas_generator('GT2',
                         p_nom=25.,
                         p_min_pu=0.4,
                         p_max_pu=0.9,
-                        min_uptime=1/6, # minutes
-                        min_downtime=1/2, # minutes
+                        min_uptime=10, # minutes 1/6 hours
+                        min_downtime=30, # minutes 1/2 hours
                         ramp_up_limit=0.12,
                         ramp_down_limit=0.12,
                         start_up_cost=70.,
@@ -73,8 +73,8 @@ network.add_gas_generator('GT3',
                         p_nom=25.,
                         p_min_pu=0.4,
                         p_max_pu=0.9,
-                        min_uptime=1/6, # minutes
-                        min_downtime=1/2, # minutes
+                        min_uptime=10, # minutes 1/6 hours
+                        min_downtime=30, # minutes 1/2 hours
                         ramp_up_limit=0.12,
                         ramp_down_limit=0.12,
                         start_up_cost=70.,
@@ -89,8 +89,8 @@ network.add_gas_generator('GT4',
                         p_nom=25.,
                         p_min_pu=0.4,
                         p_max_pu=0.9,
-                        min_uptime=1/6, # minutes
-                        min_downtime=1/2, # minutes
+                        min_uptime=10, # minutes 1/6 hours
+                        min_downtime=30, # minutes 1/2 hours
                         ramp_up_limit=0.12,
                         ramp_down_limit=0.12,
                         start_up_cost=70.,
@@ -117,14 +117,14 @@ wind_speed = np.array(wind_speed_data[:len(load)]).T[0]
 
 wind_penetration = 0.35
 
-network.add_wind_generator('WT1',
-                            p_nom=15,
-                            number_of_turbines=2,
-                            wind_speed_array=wind_speed,
-                            wind_penetration=wind_penetration,
-                            # electromechanical_conversion_efficiency=0.965
-                            electromechanical_conversion_efficiency=1.
-                            )
+# network.add_wind_generator('WT1',
+#                             p_nom=15,
+#                             number_of_turbines=2,
+#                             wind_speed_array=wind_speed,
+#                             wind_penetration=wind_penetration,
+#                             # electromechanical_conversion_efficiency=0.965
+#                             electromechanical_conversion_efficiency=1.
+#                             )
 
 # network.add_wind_generator('WT2',
 #                             p_nom=15,
@@ -171,7 +171,7 @@ network.plot_results(display_plot=True,
                     st_colors=list(['#7d0e79', 'r'])
                     )
 
-network.export_results_to_xlsx('hours.xlsx', 
+network.export_results_to_xlsx('minutes2.xlsx', 
                             include_status=True, 
                             include_means=True, 
                             compact_format=True

generators.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from scipy.optimize import curve_fit
 
+# For the fuel net heating, the reference is https://www.efunda.com/forum/show_message.cfm?start=1&thread=2009&id=2009
+
 class Generator:
     
     def __init__(self, name, carrier, p_nom, p_min_pu=0., p_max_pu=1.):
@@ -19,7 +21,7 @@ class Generator:
 
 class GasGenerator(Generator):
     
-    def __init__(self, name, carrier, p_nom, p_min_pu=0., p_max_pu=1., min_uptime=0., min_downtime=0., ramp_up_limit=1., ramp_down_limit=1., start_up_cost=0., shut_down_cost=0., efficiency_curve=None, fuel_price=1., constant_efficiency=False, co2_per_mw=0.517, SFC=0.215, inertia_constant=3.2, unavailable_snapshots=[]):
+    def __init__(self, name, carrier, p_nom, p_min_pu=0., p_max_pu=1., min_uptime=0., min_downtime=0., ramp_up_limit=1., ramp_down_limit=1., start_up_cost=0., shut_down_cost=0., efficiency_curve=None, fuel_price=1., constant_efficiency=False, co2_per_mw=0.517, fuel_net_heating=21500, inertia_constant=3.2, unavailable_snapshots=[]):
         # Parameters related to the efficiency curve, fuel consumption and co2 emissions
         super().__init__(name, 'gas', p_nom, p_min_pu, p_max_pu)
         
@@ -33,7 +35,7 @@ class GasGenerator(Generator):
         self.fuel_price = fuel_price
         self.constant_efficiency = constant_efficiency
         self.co2_per_mw = co2_per_mw
-        self.SFC = SFC
+        self.fuel_net_heating = fuel_net_heating # in BTU/lb
         self.inertia_constant = inertia_constant
         self.unavailable_snapshots = unavailable_snapshots
     

network.py
@@ -1,3 +1,4 @@
+import pyomo
 from pyomo.environ import *
 import numpy as np
 import matplotlib.pyplot as plt
@@ -45,9 +46,9 @@ class Network:
         step_load = nominal_load * np.concatenate((step_load, [step[-1]+number_of_steps-1]))
         return list(step_load)
 
-    def add_gas_generator(self, name, p_nom, p_min_pu, p_max_pu, min_uptime, min_downtime, ramp_up_limit, ramp_down_limit, start_up_cost, shut_down_cost, efficiency_curve, fuel_price, constant_efficiency=False, co2_per_mw=0.517, SFC=0.215, inertia_constant=3.2, unavailable_snapshots=[], **kwargs):
+    def add_gas_generator(self, name, p_nom, p_min_pu, p_max_pu, min_uptime, min_downtime, ramp_up_limit, ramp_down_limit, start_up_cost, shut_down_cost, efficiency_curve, fuel_price, constant_efficiency=False, co2_per_mw=0.517, fuel_net_heating=21500, inertia_constant=3.2, unavailable_snapshots=[], **kwargs):
         # Create GasGenerator object with input parameters
-        gas_generator = GasGenerator(name, 'gas', p_nom, p_min_pu, p_max_pu, min_uptime, min_downtime, ramp_up_limit, ramp_down_limit, start_up_cost, shut_down_cost, efficiency_curve=efficiency_curve, fuel_price=fuel_price, constant_efficiency=constant_efficiency, co2_per_mw=co2_per_mw, SFC=SFC, inertia_constant=inertia_constant, unavailable_snapshots=unavailable_snapshots)
+        gas_generator = GasGenerator(name, 'gas', p_nom, p_min_pu, p_max_pu, min_uptime, min_downtime, ramp_up_limit, ramp_down_limit, start_up_cost, shut_down_cost, efficiency_curve=efficiency_curve, fuel_price=fuel_price, constant_efficiency=constant_efficiency, co2_per_mw=co2_per_mw, fuel_net_heating=fuel_net_heating, inertia_constant=inertia_constant, unavailable_snapshots=unavailable_snapshots)
         
         # Set gas generator according to the load
         gas_generator.initialize(self.load)
@@ -967,29 +968,52 @@ class Network:
             for j in self.snapshots:
                 # Initialize emissions at each snapshot
                 fuel_sum = 0
-                
+                                
                 # Iterate over gas generators:
                 for gen in self.gas_generators:
                     # Get dispatch in MW for given generator
                     gen_p = self.model.generator_p[gen.name,j].value
                     
-                    if gen_p < 1e-5:
-                        gen_p = 0
-                        
-                    # Calculate fuel consumption
-                    fuel = gen.SFC * gen_p * time_factor
+                    # Calculate dispatch per unit for given generator
+                    gen_p_pu = gen_p / gen.p_nom
                     
-                    # Sum fuel of given generator to total fuel at snapshot j
+                    # Get efficiency parameters to calculate efficiency at each snapshot
+                    ef_a = gen.ef_a
+                    ef_b = gen.ef_b
+                    ef_k = gen.ef_k
+                    
+                    # Calculate efficiency at each snapshot
+                    if gen.constant_efficiency:
+                        efficiency = ef_k
+                    else:
+                        efficiency = ef_a * gen_p_pu**2 + ef_b * gen_p_pu
+                    
+                    # Calculate power provided by the fuel in MW
+                    if efficiency < 1e-3:
+                        fuel_p = 0
+                    else:
+                        fuel_p = gen_p/efficiency
+                    
+                    # Convert fuel net heating to fuel ton per input power
+                    BTU_per_pound = gen.fuel_net_heating
+                    BTU_to_MWh = 1/3412000
+                    pound_to_kg = 0.45359237
+                    MWh_per_kg = BTU_per_pound * BTU_to_MWh / pound_to_kg
+                    kg_per_MWh = 1/MWh_per_kg
+
+                    # Calculate emission
+                    fuel = fuel_p * kg_per_MWh * time_factor
+                    
+                    # Sum emissions of given generator to total emissions at snapshot j
                     fuel_sum += fuel
                     
                 # Update emissions array with calculated emission
                 fuel_array[j] = fuel_sum
-                
+            
             # Save emissions
             self.fuel = pd.DataFrame(fuel_array, columns = ['fuel'])
             return
-        return
-    
+
     def save_state_of_charge(self):
         if self.is_solved:
             # Initialize state of charge array
@@ -1290,7 +1314,7 @@ class Network:
             
             # Add Fuel and Total fuel to data container
             data['Fuel [kg]'] = list(self.fuel['fuel'])
-            data['Total fuel [kg]'] = np.sum(data['Fuel [kg]'])
+            data['Total fuel [ton]'] = np.sum(data['Fuel [kg]']) / 1000
             
             # Add ROCOF to data container
             data['ROCOF [Hz/s]'] = list(self.rocof['ROCOF [Hz/s]'])